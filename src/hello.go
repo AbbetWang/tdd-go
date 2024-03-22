6eb17e592abe66227ca738240854f183ddc5ae15
linux <linux@40.com>
2024-03-22 14:55:25 +0800

    add: support new language

--- src/hello.go
+++ src/hello.go
@@ -3,15 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	x := Hello("world")
+	x := Hello("world", "")
 	fmt.Println(x)
 }
 
 const englishHelloPrefix = "Hello, "
 
-func Hello(name string) string {
-	if "" == name {
+func Hello(name string, language string) string {
+	if name == "" {
 		name = "World"
+	}
+	if language == "Spanish" {
+		return "Hola, " + name
 	}
 	return englishHelloPrefix + name
 }
