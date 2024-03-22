46d7124c0eb71e4ed36a528bfbba1f754d5169ac
linux <linux@40.com>
2024-03-22 17:50:27 +0800

    refactor:delete redundant test

--- src/arrays/sum_test.go
+++ src/arrays/sum_test.go
@@ -3,13 +3,14 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
 
-	numbers := [5]int{1, 2, 3, 4, 5}
+		got := Sum(numbers)
+		want := 6
 
-	got := Sum(numbers)
-	want := 15
-
-	if got != want {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
-	}
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
 }
