330ece0a4c4a589e1c15a8d0028005be25d74646
AbbetWang <AbbetWang@gmail.com>
2024-03-23 22:55:33 +0800

    refactor: refactor

--- src/arrays/sum_test.go
+++ src/arrays/sum_test.go
@@ -33,21 +33,21 @@ func ExampleSum() {
 	// Output: 22
 }
 func TestSumAllTails(t *testing.T) {
-	t.Run("make the sums of some slices", func(t *testing.T) {
-		got := SumAllTails([]int{1, 2}, []int{0, 9})
-		want := []int{2, 9}
-
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got %v want %v", got, want)
 		}
+	}
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		checkSums(t, got, want)
 	})
 
 	t.Run("safely sum empty slices", func(t *testing.T) {
 		got := SumAllTails([]int{}, []int{3, 4, 5})
 		want := []int{0, 9}
-
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		checkSums(t, got, want)
 	})
 }
