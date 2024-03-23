d90cf99bbdef4cad5b55ed6f392bf3bab11f9145
AbbetWang <AbbetWang@gmail.com>
2024-03-23 22:39:44 +0800

    add: sumalltails update

--- src/arrays/sum.go
+++ src/arrays/sum.go
@@ -14,3 +14,14 @@ func SumAll(numbersToSum ...[]int) []int {
 	}
 	return sums
 }
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		var tail []int
+		if len(numbers) > 0 {
+			tail = numbers[1:]
+		}
+		sums = append(sums, Sum(tail))
+	}
+	return sums
+}
