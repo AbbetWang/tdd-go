997b2901c8ca04b78f336617d50a5a8c28dff065
AbbetWang <AbbetWang@gmail.com>
2024-03-23 23:41:25 +0800

    refactor: test use t.run

--- src/structs-methods-interfaces/shage.go
+++ src/structs-methods-interfaces/shage.go
@@ -1,13 +1,21 @@
 package structsmethodsinterfaces
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
+}
+type Circle struct {
+	Radius float64
 }
 
 func Perimeter(rectoangle Rectangle) float64 {
 	return 2 * (rectoangle.Width + rectoangle.Height)
 }
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
--- src/structs-methods-interfaces/shape_test.go
+++ src/structs-methods-interfaces/shape_test.go
@@ -21,10 +21,18 @@ func checkRectangles(got float64, want float64, t testing.TB) {
 func TestArea(t *testing.T) {
 	t.Run("rectangles", func(t *testing.T) {
 		rectangle := Rectangle{12.0, 6.0}
-		got := Area(rectangle)
+		got := rectangle.Area()
 		want := 72.0
 		if got != want {
 			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
 		}
 	})
 }
