17dc4d2d44f6b0f04f28463f761a9595c9ed45b7
AbbetWang <AbbetWang@gmail.com>
2024-03-23 23:20:50 +0800

    refactor: use rectangle type struct

--- src/structs-methods-interfaces/shage.go
+++ src/structs-methods-interfaces/shage.go
@@ -1,7 +1,12 @@
 package structsmethodsinterfaces
 
-func Perimeter(width, height float64) float64 {
-	return 2 * (width + height)
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func Perimeter(rectoangle Rectangle) float64 {
+	return 2 * (rectoangle.Width + rectoangle.Height)
 }
 func Area(width, height float64) float64 {
 	return width * height
--- src/structs-methods-interfaces/shape_test.go
+++ src/structs-methods-interfaces/shape_test.go
@@ -4,12 +4,9 @@ import "testing"
 
 func TestPerimeter(t *testing.T) {
 	t.Run("rectangles", func(t *testing.T) {
-		t.Helper()
-		got := Perimeter(10.0, 10.0)
+		rectangle := Rectangle{10.0, 10.0}
+		got := Perimeter(rectangle)
 		want := 40.0
-		checkRectangles(got, want, t)
-		got = Perimeter(15.0, 10.0)
-		want = 50.0
 		checkRectangles(got, want, t)
 	})
 }
