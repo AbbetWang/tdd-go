4da8f8e9ac5893b86ee99fde8e5e55223b8cc78a
AbbetWang <AbbetWang@gmail.com>
2024-03-24 23:40:35 +0800

    refactor: 重构

--- src/points/wallet.go
+++ src/points/wallet.go
@@ -1,13 +1,23 @@
 package points
 
+import "fmt"
+
+type Bitcoin int
+
 type Wallet struct {
-	balance int
+	balance Bitcoin
+}
+type Stringer interface {
+	String() string
 }
 
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance = w.balance + amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
