85d99f5a785c9ce9ff847641e29103dd3a85e950
linux <linux@40.com>
2024-03-25 11:05:25 +0800

    refactor

--- src/points/wallet_test.go
+++ src/points/wallet_test.go
@@ -16,7 +16,8 @@ func TestWallet(t *testing.T) {
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 
-		wallet.Withdraw(Bitcoin(10))
+		err := wallet.Withdraw(Bitcoin(10))
+		assertNoError(t, err)
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 	t.Run("withdraw insufficient funds", func(t *testing.T) {
@@ -34,6 +35,12 @@ func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
 	got := wallet.Balance()
 	if got != want {
 		t.Errorf("got %s want %s", got, want)
+	}
+}
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
 	}
 }
 func assertError(t testing.TB, got, want error) {
