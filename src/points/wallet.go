5d3a56f8393942eb1af853ff4dada57034ce7f43
linux <linux@40.com>
2024-03-25 10:09:51 +0800

    add: support exception in withdraw

--- src/points/wallet.go
+++ src/points/wallet.go
@@ -1,6 +1,9 @@
 package points
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Bitcoin int
 
@@ -18,8 +21,12 @@ func (w *Wallet) Deposit(amount Bitcoin) {
 func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
-func (w *Wallet) Withdraw(amount Bitcoin) {
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return errors.New("oh no")
+	}
 	w.balance = w.balance - amount
+	return nil
 }
 func (b Bitcoin) String() string {
 	return fmt.Sprintf("%d BTC", b)
--- src/points/wallet_test.go
+++ src/points/wallet_test.go
@@ -6,31 +6,37 @@ import (
 
 func TestWallet(t *testing.T) {
 
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 
 		wallet.Deposit(Bitcoin(10))
-
-		got := wallet.Balance()
-
 		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, want)
 	})
 
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 
 		wallet.Withdraw(Bitcoin(10))
-
-		got := wallet.Balance()
-
 		want := Bitcoin(10)
+		assertBalance(t, wallet, want)
+	})
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
 
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
+		assertBalance(t, wallet, startingBalance)
+
+		if err == nil {
+			t.Error("wanted an error but didn't get one")
 		}
 	})
 }
