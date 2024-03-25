3c886b6a876bf15d96154a8f1cd2640435b5e6e5
linux <linux@40.com>
2024-03-25 09:21:05 +0800

    add: support withdraw

--- src/points/wallet.go
+++ src/points/wallet.go
@@ -18,6 +18,9 @@ func (w *Wallet) Deposit(amount Bitcoin) {
 func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance = w.balance - amount
+}
 func (b Bitcoin) String() string {
 	return fmt.Sprintf("%d BTC", b)
 }
