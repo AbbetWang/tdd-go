9ce6d90f752909cbd9069ac9a86f71cb6324b1ce
AbbetWang <AbbetWang@gmail.com>
2024-03-26 23:14:08 +0800

    refactor:

--- src/maps/dictionary_test.go
+++ src/maps/dictionary_test.go
@@ -21,12 +21,13 @@ func TestSearch(t *testing.T) {
 	})
 }
 func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	word := "test"
-	definition := "this is just a test"
-	dictionary.Add(word, definition)
-
-	assertDefinition(t, dictionary, word, definition)
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
+		dictionary.Add(word, definition)
+		assertDefinition(t, dictionary, word, definition)
+	})
 }
 
 func assertDefinition(t testing.TB, dictionary Dictionary, word string, definition string) {
