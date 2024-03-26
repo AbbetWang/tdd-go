81d0c3d649243483a5f2272c552a6624a1256d04
linux <linux@40.com>
2024-03-26 09:49:49 +0800

    refactor:

--- src/maps/dictionary_test.go
+++ src/maps/dictionary_test.go
@@ -4,11 +4,12 @@ import "testing"
 
 func TestSearch(t *testing.T) {
 	dictionary := map[string]string{"test": "this is just a test", "a": "this is just a a"}
-
-	got := Search(dictionary, "test")
-	assertEqual(t, got, "this is just a test")
-	got = Search(dictionary, "a")
-	assertEqual(t, got, "this is just a a")
+	t.Run("happy path", func(t *testing.T) {
+		got := Search(dictionary, "test")
+		assertEqual(t, got, "this is just a test")
+		got = Search(dictionary, "a")
+		assertEqual(t, got, "this is just a a")
+	})
 }
 
 func assertEqual(t testing.TB, got, want string) {
