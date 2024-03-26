a4dc6bbff00dddbd0a434a86b10e8c648578ac7f
linux <linux@40.com>
2024-03-26 10:23:58 +0800

    support unknown word

--- src/maps/dictionary_test.go
+++ src/maps/dictionary_test.go
@@ -5,14 +5,21 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test", "a": "this is just a a"}
 	t.Run("happy path", func(t *testing.T) {
-		got := dictionary.Search("test")
+		got, _ := dictionary.Search("test")
 		assertStrings(t, got, "this is just a test")
-		got = dictionary.Search("a")
+		got, _ = dictionary.Search("a")
 		assertStrings(t, got, "this is just a a")
 	})
-	// t.Run("sad path,if a key given is not in the map", func(t *testing.T) {
-	// 	got := Search(dictionary, "b")
-	// })
+	t.Run("sad path,if a key given is not in the map", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := "could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("expected to get an error.")
+		}
+
+		assertStrings(t, err.Error(), want)
+	})
 }
 
 func assertStrings(t testing.TB, got, want string) {
