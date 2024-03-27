58b3170045fec0e9850668c38499c5f686a274f5
linux <linux@40.com>
2024-03-27 11:01:58 +0800

    update:support pauses

--- src/mock/count.go
+++ src/mock/count.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 func main() {
@@ -16,6 +17,7 @@ const countdownStart = 3
 func Countdown(out io.Writer) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
+		time.Sleep(1 * time.Second)
 	}
 	fmt.Fprint(out, finalWorld)
 }
