0f4921e48743229f6ad9c44c4ddfbc064a9fb89c
linux <linux@40.com>
2024-03-27 10:18:43 +0800

    add:new http greet

--- src/dp/hello.go
+++ src/dp/hello.go
@@ -3,12 +3,17 @@ package main
 import (
 	"fmt"
 	"io"
-	"os"
+	"log"
+	"net/http"
 )
 
 func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
 }
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
 func main() {
-	Greet(os.Stdout, "Elodie")
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
+
 }
