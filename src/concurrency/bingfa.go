6be3455061382858f3185e1167c7757021928a51
linux <linux@40.com>
2024-03-28 10:57:42 +0800

    add: add a test for select

--- src/concurrency/bingfa.go
+++ src/concurrency/bingfa.go
@@ -1,11 +1,22 @@
 package concurrency
 
 type WebsiteChecker = func(url string) bool
+type result struct {
+	string
+	bool
+}
 
-func CheckWebsites(wc WebsiteChecker, websites []string) map[string]bool {
+func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
-	for _, url := range websites {
-		results[url] = wc(url)
+	resultChannel := make(chan result)
+	for _, url := range urls {
+		go func(u string) {
+			resultChannel <- result{u, wc(u)}
+		}(url)
+	}
+	for i := 0; i < len(urls); i++ {
+		r := <-resultChannel
+		results[r.string] = r.bool
 	}
 	return results
 }
--- src/concurrency/bingfa_test.go
+++ src/concurrency/bingfa_test.go
@@ -3,6 +3,7 @@ package concurrency
 import (
 	"reflect"
 	"testing"
+	"time"
 )
 
 func mockWebsiteChecker(url string) bool {
@@ -29,5 +30,20 @@ func TestCheckWebsites(t *testing.T) {
 
 	if !reflect.DeepEqual(want, got) {
 		t.Fatalf("wanted %v, got %v", want, got)
+	}
+}
+func slowStubWebsiteChecker(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
+	return true
+}
+
+func BenchmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "a url"
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		CheckWebsites(slowStubWebsiteChecker, urls)
 	}
 }
