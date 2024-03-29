17a2e4b7146656860d60d911808df2179a9c7144
linux <linux@40.com>
2024-03-29 10:15:04 +0800

    refactor

--- src/concurrency/race_test.go
+++ src/concurrency/race_test.go
@@ -26,13 +26,11 @@ func TestRacer(t *testing.T) {
 		}
 	})
 	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
-		serverA := makeDelayedServer(11 * time.Second)
-		serverB := makeDelayedServer(12 * time.Second)
+		serverA := makeDelayedServer(25 * time.Millisecond)
 
 		defer serverA.Close()
-		defer serverB.Close()
 
-		_, err := Racer(serverA.URL, serverB.URL)
+		_, err := ConfigurableRacer(serverA.URL, serverA.URL, 20*time.Millisecond)
 
 		if err == nil {
 			t.Error("expected an error but didn't get one")
