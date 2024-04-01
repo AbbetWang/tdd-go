3c3ea19489d9e8b3bebe87749261d582929c4d98
linux <linux@40.com>
2024-04-01 10:25:40 +0800

    update: support concurrency

--- src/sync/sync.go
+++ src/sync/sync.go
@@ -1,10 +1,15 @@
 package sync
 
+import "sync"
+
 type Counter struct {
+	mu    sync.Mutex
 	count int64
 }
 
 func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.count++
 }
 func (c *Counter) Value() (value int64) {
--- src/sync/sync_test.go
+++ src/sync/sync_test.go
@@ -1,6 +1,9 @@
 package sync
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestCounter(t *testing.T) {
 	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
@@ -11,11 +14,28 @@ func TestCounter(t *testing.T) {
 		want := 3
 		assertCounter(t, counter, want)
 	})
+	t.Run("it runs safely concurrently", func(t *testing.T) {
+		wantedCount := 1000
+		counter := Counter{}
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func() {
+				counter.Inc()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+
+		assertCounter(t, counter, wantedCount)
+	})
 }
 
 func assertCounter(t testing.TB, counter Counter, want int) {
 	t.Helper()
-	if counter.Value() != 3 {
+	if counter.Value() != int64(want) {
 		t.Errorf("got %d, want %d", counter.Value(), want)
 	}
 }
