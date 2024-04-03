ff4af9bd124b8bbddc003fb38b41550fd7dc00cc
linux <linux@40.com>
2024-04-03 10:41:36 +0800

    update: support II

--- src/property/numeral.go
+++ src/property/numeral.go
@@ -1,5 +1,12 @@
 package property
 
 func ConvertToRoman(arabic int) string {
-	return "I"
+
+	result := ""
+	for i := 0; i < arabic; i++ {
+		result += "I"
+	}
+
+	return result
+
 }
--- src/property/numeral_test.go
+++ src/property/numeral_test.go
@@ -2,10 +2,22 @@ package property
 
 import "testing"
 
-func TestRomanNumberals(t *testing.T) {
-	got := ConvertToRoman(1)
-	want := "I"
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+func TestRomanNumerals(t *testing.T) {
+	t.Run("1 gets converted to I", func(t *testing.T) {
+		got := ConvertToRoman(1)
+		want := "I"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("2 gets converted to II", func(t *testing.T) {
+		got := ConvertToRoman(2)
+		want := "II"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
 }
