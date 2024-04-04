b1faa736cf3dc7d58d573d30b42006cad139da32
AbbetWang <AbbetWang@gmail.com>
2024-04-04 21:27:31 +0800

    add:secondHandPoint

--- src/clockface/clockface_test.go
+++ src/clockface/clockface_test.go
@@ -26,6 +26,24 @@ func TestSecondsInRadians(t *testing.T) {
 		})
 	}
 }
+
+func TestSecondHandPoint(t *testing.T) {
+	cases := []struct {
+		time  time.Time
+		point Point
+	}{
+		{simpleTime(0, 0, 30), Point{0, -1}},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			got := secondHandPoint(c.time)
+			if got != c.point {
+				t.Fatalf("Wanted %v Point, but got %v", c.point, got)
+			}
+		})
+	}
+}
 func simpleTime(hours, minutes, seconds int) time.Time {
 	return time.Date(312, time.October, 28, hours, minutes, seconds, 0, time.UTC)
 }
