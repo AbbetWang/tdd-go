cf4d5a87d489e33dd92e311dc30aa069ac283ec0
AbbetWang <AbbetWang@gmail.com>
2024-04-04 21:10:09 +0800

    fixed:

--- src/clockface/clockface.go
+++ src/clockface/clockface.go
@@ -15,5 +15,5 @@ func SecondHand(t time.Time) Point {
 	return Point{X: 150, Y: 150 - 90}
 }
 func secondsInRadians(t time.Time) float64 {
-	return math.Pi
+	return (math.Pi / (30 / (float64(t.Second()))))
 }
