d6c636464fd6cac3da5b700bb1c09c59d5789381
AbbetWang <AbbetWang@gmail.com>
2024-04-06 08:36:55 +0800

    add: minute test

--- src/clockface/clockface_test.go
+++ src/clockface/clockface_test.go
@@ -6,6 +6,23 @@ import (
 	"time"
 )
 
+func TestMinutesInRadians(t *testing.T) {
+	cases := []struct {
+		time  time.Time
+		angle float64
+	}{
+		{simpleTime(0, 30, 0), math.Pi},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			got := minutesInRadians(c.time)
+			if got != c.angle {
+				t.Fatalf("Wanted %v radians, but got %v", c.angle, got)
+			}
+		})
+	}
+}
 func TestSecondsInRadians(t *testing.T) {
 	cases := []struct {
 		time  time.Time
