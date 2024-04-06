895ffafaf2a7bd7da04969db3044273b7b00055c
AbbetWang <AbbetWang@gmail.com>
2024-04-06 08:28:10 +0800

    refactor: matched types

--- src/clockface/clockface_acceptance_test.go
+++ src/clockface/clockface_acceptance_test.go
@@ -9,29 +9,28 @@ import (
 	"example.com/tdd-go/src/clockface"
 )
 
-type Svg struct {
+type SVG struct {
 	XMLName xml.Name `xml:"svg"`
-	Text    string   `xml:",chardata"`
 	Xmlns   string   `xml:"xmlns,attr"`
 	Width   string   `xml:"width,attr"`
 	Height  string   `xml:"height,attr"`
 	ViewBox string   `xml:"viewBox,attr"`
 	Version string   `xml:"version,attr"`
-	Circle  struct {
-		Text  string `xml:",chardata"`
-		Cx    string `xml:"cx,attr"`
-		Cy    string `xml:"cy,attr"`
-		R     string `xml:"r,attr"`
-		Style string `xml:"style,attr"`
-	} `xml:"circle"`
-	Line []struct {
-		Text  string `xml:",chardata"`
-		X1    string `xml:"x1,attr"`
-		Y1    string `xml:"y1,attr"`
-		X2    string `xml:"x2,attr"`
-		Y2    string `xml:"y2,attr"`
-		Style string `xml:"style,attr"`
-	} `xml:"line"`
+	Circle  Circle   `xml:"circle"`
+	Line    []Line   `xml:"line"`
+}
+
+type Circle struct {
+	Cx float64 `xml:"cx,attr"`
+	Cy float64 `xml:"cy,attr"`
+	R  float64 `xml:"r,attr"`
+}
+
+type Line struct {
+	X1 float64 `xml:"x1,attr"`
+	Y1 float64 `xml:"y1,attr"`
+	X2 float64 `xml:"x2,attr"`
+	Y2 float64 `xml:"y2,attr"`
 }
 
 func TestSecondHandAtMidnight(t *testing.T) {
@@ -61,11 +60,11 @@ func TestSVGWriterAtMidnight(t *testing.T) {
 	b := bytes.Buffer{}
 	clockface.SVGWriter(&b, tm)
 
-	svg := Svg{}
+	svg := SVG{}
 	xml.Unmarshal(b.Bytes(), &svg)
 
-	x2 := "150.000"
-	y2 := "60.000"
+	x2 := 150.000
+	y2 := 60.000
 
 	for _, line := range svg.Line {
 		if line.X2 == x2 && line.Y2 == y2 {
