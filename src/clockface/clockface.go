740ab4088b25e1318c773390eb50a14611ca5465
linux <linux@40.com>
2024-04-09 10:40:09 +0800

    add: hour hand compelete

--- src/clockface/clockface.go
+++ src/clockface/clockface.go
@@ -50,3 +50,6 @@ func hoursInRadians(t time.Time) float64 {
 	return (minutesInRadians(t) / 12) +
 		(math.Pi / (6 / float64(t.Hour()%12)))
 }
+func hourHandPoint(t time.Time) Point {
+	return angleToPoint(hoursInRadians(t))
+}
