a59a1a196113906688ac9227bc8af6b5cfbd73b2
linux <linux@40.com>
2024-04-15 15:25:00 +0800

    refactor:

--- src/blogposts/post.go
+++ src/blogposts/post.go
@@ -27,14 +27,19 @@ func newPost(postFile io.Reader) (Post, error) {
 
 	title := readMetaLine(titleSeparator)
 	description := readMetaLine(descriptionSeparator)
-	tags := strings.Split(readMetaLine(tagsSeparator), ",")
-	for i, tag := range tags {
-		tags[i] = strings.TrimSpace(tag)
-
-	}
+	tags := getTags(readMetaLine)
 	return Post{
 		Title:       title,
 		Description: description,
 		Tags:        tags,
 	}, nil
+}
+
+func getTags(readMetaLine func(tagName string) string) []string {
+	tags := strings.Split(readMetaLine(tagsSeparator), ",")
+	for i, tag := range tags {
+		tags[i] = strings.TrimSpace(tag)
+
+	}
+	return tags
 }
