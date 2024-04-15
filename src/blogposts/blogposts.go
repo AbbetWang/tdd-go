6600d8215a0fd5f5fcef5c287a921b0ecf4f2b85
linux <linux@40.com>
2024-04-15 14:33:59 +0800

    refactor

--- src/blogposts/blogposts.go
+++ src/blogposts/blogposts.go
@@ -30,15 +30,12 @@ func getPost(fileSystem fs.FS, f fs.DirEntry) (Post, error) {
 		return Post{}, err
 	}
 	defer postFile.Close()
-	return newPost(postFile)
 
-}
-
-func newPost(postFile fs.File) (Post, error) {
 	postData, err := io.ReadAll(postFile)
 	if err != nil {
 		return Post{}, err
 	}
 	post := Post{Title: string(postData)[7:]}
 	return post, nil
+
 }
