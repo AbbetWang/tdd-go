9a04c4c18e4f0c250fe6d4e61262a097c43d3a23
linux <linux@40.com>
2024-04-15 14:35:50 +0800

    refactor

--- src/blogposts/blogposts.go
+++ src/blogposts/blogposts.go
@@ -16,7 +16,7 @@ func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
 	}
 	var posts []Post
 	for _, f := range dir {
-		post, err := getPost(fileSystem, f)
+		post, err := getPost(fileSystem, f.Name())
 		if err != nil {
 			return nil, err
 		}
@@ -24,18 +24,22 @@ func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
 	}
 	return posts, nil
 }
-func getPost(fileSystem fs.FS, f fs.DirEntry) (Post, error) {
-	postFile, err := fileSystem.Open(f.Name())
+func getPost(fileSystem fs.FS, fileName string) (Post, error) {
+	postFile, err := fileSystem.Open(fileName)
 	if err != nil {
 		return Post{}, err
 	}
 	defer postFile.Close()
 
+	return newPost(postFile)
+
+}
+
+func newPost(postFile io.Reader) (Post, error) {
 	postData, err := io.ReadAll(postFile)
 	if err != nil {
 		return Post{}, err
 	}
 	post := Post{Title: string(postData)[7:]}
 	return post, nil
-
 }
